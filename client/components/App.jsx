5110fcffd6da395232553f292f075a7b2e7edf47
EDA <eda@mac-mini-4.local>
2017-01-27 12:52:27 +1300

    add homepage components

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 
+import Thumbnails from './Thumbnails'
+
+const posts = []
+
 const App = () => (
   <div>
     <h1>Sophora</h1>
+    <Thumbnails posts={posts} />
   </div>
 )
 
---
+++ client/components/PostButton.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export default React.createClass({
+  render() {
+    return  (
+      <div className="row">
+        <div className="col-md-2">
+          <a href="/add-post.html"><button type="button" name="button" className="btn pull-right">+</button></a>
+        </div>
+      </div>
+          )
+  }
+})
