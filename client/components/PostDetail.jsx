e9f01e4e99a6d5485b9a3888fdd28e45dda15506
EDA <eda@mac-mini-2.local>
2017-01-27 14:38:37 +1300

    Add post nav links to PostDetail

--- client/components/PostDetail.jsx
+++ client/components/PostDetail.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import React, { PropTypes } from 'react'
 
 import Captions from './Captions'
 import Post from './Post'
 
-export default () => {
+const PostDetail = props => {
+  const postId = props.location.params.postId
+  const captionId = props.location.params.captionId
+  const post = props.posts.find(post => post.id === postId)
+  const caption = post.captions.find(caption => caption.id === captionId)
   return (
     <div>
-      <Post />
-      <Captions />
+      <Post post={post} numPosts={props.posts.length} />
+      <Captions caption={caption} />
     </div>
   )
 }
+
+PostDetail.propTypes = {
+  location: PropTypes.shape({
+    params: PropTypes.shape({
+      captionId: PropTypes.number.isRequired,
+      postId: PropTypes.number.isRequired
+    }).isRequired
+  }).isRequired,
+  posts: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      captions: PropTypes.arrayOf({
+        id: PropTypes.number.isRequired
+      }).isRequired
+    })
+  ).isRequired
+}
+
+export default PostDetail
--- client/components/PostNav.jsx
+++ client/components/PostNav.jsx
@@ -1,15 +1,28 @@
 import React, { PropTypes } from 'react'
+import { Link } from 'react-redux'
 
-const PostNav = ({ direction }) => {
-  return (
-    <button className='btn'>
+const PostNav = ({ direction, enabled, postId }) => {
+  const Button = (
+    <button className={`btn ${enabled ? '' : 'disabled'}`}>
       <span className={`fa fa-arrow-${direction}`} aria-hidden='true' />
     </button>
   )
+  if (enabled) {
+    const newPostId = postId + (direction === 'left' ? -1 : 1)
+    return (
+      <Link to={`/posts/${newPostId}`}>
+        <Button />
+      </Link>
+    )
+  } else {
+    return Button
+  }
 }
 
 PostNav.propTypes = {
-  direction: PropTypes.string.isRequired
+  direction: PropTypes.string.isRequired,
+  enabled: PropTypes.bool.isRequired,
+  postId: PropTypes.number.isRequired
 }
 
 export default PostNav
