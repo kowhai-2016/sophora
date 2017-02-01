dd26ed2671e5dff1427bf9b1ded5a7e85814611a
EDA <eda@mac-mini-4.local>
2017-02-02 10:44:48 +1300

    resolve #33

--- client/components/CaptionAdd.jsx
+++ client/components/CaptionAdd.jsx
@@ -2,7 +2,7 @@ import React, { PropTypes } from 'react'
 import { Link } from 'react-router'
 
 const CaptionAdd = ({ postId }) => {
-  return (
+    return (
     <Link to={`posts/${postId}/add`}>
       <button className='btn'>
         <span className='fa fa-plus' />
