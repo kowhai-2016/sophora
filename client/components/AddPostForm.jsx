e338424e7ba689996e0731eabd3f77a59f7c7de5
EDA <eda@mac-mini-3.local>
2017-02-02 16:57:30 +1300

    Did some styling

--- client/components/AddPostForm.jsx
+++ client/components/AddPostForm.jsx
@@ -4,14 +4,18 @@ import { Link } from 'react-router'
 export default React.createClass({
   render () {
     return (
-      <div className="row add-post-form">
-        <form style={{width: '100%'}}>
-          <div className="form-group">
-            <label htmlFor="url">Image URL:</label>
-            <input name='url' type="url" className="form-control" id="url" placeholder="Place your image URL here" style={{width: '100%'}} />
-          </div>
-          <Link role="button" to="/posts/1/add" className="btn btn-primary pull-right">Add Post</Link>
-        </form>
+      <div className="container">
+        <div className="row add-post-form">
+          <form>
+            <div className="form-group">
+              <label htmlFor="url">Image URL:</label>
+              <input className="url-form" name='url' type="url" id="url" placeholder="Place your image URL here"/>
+              <div className="row post-button">
+                <Link role="button" to="/posts/1/add" className="btn btn-primary pull-right">Add Post</Link>
+              </div>
+            </div>
+          </form>
+        </div>
       </div>
     )
   }
